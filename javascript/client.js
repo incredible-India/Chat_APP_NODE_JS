33caeb98e5acf93f5e58ae07b1a693f4a59c8fb8
Himsnshu Sharma <iamonlyhimanshusharma@gmail.com>
2021-05-12 23:05:50 +0530

    cors error resolved cors:{ origin : *}

--- javascript/client.js
+++ javascript/client.js
@@ -1,10 +1,13 @@
 // const io = require("socket.io-client");
 // const io = require("socket.io-client");
-const socket = new io("http://localhost:8080")
+
+
+const socket =  io("http://localhost:8000")
 // const socket = io('http://127.0.0.1:8080');
 
+// socket.emit("")
+socket.emit("userJoined","himanhi")
 
 
 
-
-socket.emit('userJoined','Username');
+socket.emit('userJoined','Username'); 
